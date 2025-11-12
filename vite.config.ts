30eb4ae2465a6508906dc807da13832c26e0198c
Drsky <contact@drsky.ovh>
2025-11-12 17:39:36 +0100

    Add Vite configuration for client and server builds
    - Introduced `vite.config.ts` to manage build settings for both client and server applications.
    - Configured plugins for React, Tailwind CSS, and TanStack Router with SSR support.
    - Defined output settings for asset management and server entry points.

---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import devServer, { defaultOptions } from '@hono/vite-dev-server'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+const ssrBuild = {
+  outDir: 'dist/server',
+  ssrEmitAssets: true,
+  copyPublicDir: false,
+  emptyOutDir: false,
+  rollupOptions: {
+    input: resolve(__dirname, 'src/entry-server.tsx'),
+    output: {
+      entryFileNames: 'index.js',
+      chunkFileNames: 'assets/[name]-[hash].js',
+      assetFileNames: 'assets/[name]-[hash][extname]',
+    },
+  },
+  ssr: true,
+}
+
+const clientBuild = {
+  outDir: 'dist/client',
+  copyPublicDir: true,
+  emptyOutDir: true,
+  rollupOptions: {
+    input: resolve(__dirname, 'src/entry-client.tsx'),
+    output: {
+      entryFileNames: 'assets/[name].js',
+      chunkFileNames: 'assets/[name]-[hash].js',
+      assetFileNames: 'assets/[name]-[hash][extname]',
+    },
+  },
+  manifest: true,
+}
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+    devServer({
+      entry: 'src/entry-server.tsx',
+      injectClientScript: false,
+      exclude: [
+        /^\/src\/.+/, // Allow Vite to handle /src/ requests
+        ...defaultOptions.exclude,
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: mode === 'client' ? clientBuild : ssrBuild,
+}))
