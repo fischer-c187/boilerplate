f9f22247f3cd65247f4405a2fa7c3eb25ae315a8
Drsky <contact@drsky.ovh>
2025-11-16 22:52:38 +0100

    refactor(routes): rename NoSRR to NoSSR and update component

--- src/front/routes/client.tsx
+++ src/front/routes/client.tsx
@@ -1,10 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/client')({
-  component: NoSRR,
+  component: NoSSR,
   ssr: false,
 })
 
-function NoSRR() {
-  return <div>test</div>
+function NoSSR() {
+  // const session = authClient.getSession()
+
+  // console.log(session)
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen h-4xl bg-red-500">
+      <p>hello world</p>
+    </div>
+  )
 }
