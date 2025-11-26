78f0be189e2f2d8b7e7a7dfccb226851c145ac7b
Drsky <contact@drsky.ovh>
2025-11-26 15:45:37 +0100

    feat(i18n): integrate i18n instance with router setup

--- src/front/entry-client.tsx
+++ src/front/entry-client.tsx
@@ -1,7 +1,11 @@
+import { createI18n, detectLanguageClient } from '@/shared/i18n/config'
 import { RouterClient } from '@tanstack/react-router/ssr/client'
 import { hydrateRoot } from 'react-dom/client'
-import { createRouter } from './router.ts'
+import { createRouter } from './router'
 
-const router = createRouter()
+const language = detectLanguageClient()
+const i18nInstance = await createI18n(language)
+
+const router = createRouter(i18nInstance)
 
 hydrateRoot(document, <RouterClient router={router} />)
